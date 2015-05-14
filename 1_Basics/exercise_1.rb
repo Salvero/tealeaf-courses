99edc79d5d7309f6bbeff7a4b5eb13d938cc0925
Salman Akram <thunder9ruby@gmail.com>
2015-05-13 22:51:07 -0400

    setup files

---
+++ 1_Basics/exercise_1.rb
@@ -0,0 +1,8 @@
+# Add Two Strings together
+first_name = "John"
+last_name = "Doe"
+
+puts "I am #{first_name} #{last_name}!"
+
+puts "I am " + first_name + " " + last_name + "!"
+
---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Exercises</h1>
+
+I've been practising with exercises from various web development and courses, always commiting all files when done in this repo.
+
