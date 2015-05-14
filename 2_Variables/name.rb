9baaf4dc2a569af0eb7d353046b9410c410c263f
Salman Akram <thunder9ruby@gmail.com>
2015-05-14 00:16:59 -0400

    more examples

--- 2_Variables/name.rb
+++ 2_Variables/name.rb
@@ -1,3 +1,5 @@
+
+# ask user name and their summer plan
 puts "Your name is ......?"
 name = gets.chomp
 puts "Okay #{name.capitalize}"
@@ -7,3 +9,19 @@ puts ""
 puts "so what is your planning to do in this summer ?!"
 message = gets.chomp
 puts "Hmmm really #{message.upcase}....that's great idea, we can do that! :)"
+
+puts ""
+
+# ten times for user name
+10.times do 
+	puts name.capitalize
+end
+
+#ask user first name and last name
+puts "Your first name is......?"
+first_name = gets.chomp.capitalize
+
+puts "and your last name?"
+last_name = gets.chomp.capitalize
+
+puts "Great! Your full name is #{first_name} #{last_name}"
