64e659374ceeb4be1cb2e1be377ad5498f76ded5
Salman Akram <thunder9ruby@gmail.com>
2015-05-14 00:03:43 -0400

    another variables

--- 2_Variables/age.rb
+++ 2_Variables/age.rb
@@ -1,14 +1,29 @@
-puts "Hi, how old are you?"
-answer = gets.chomp
+# puts "Hi, how old are you?"
+# answer = gets.chomp
 
-	puts "great.  I have some questions - In 10 years you will be ?"
-	answer1 = gets.chomp
+# 	puts "great.  I have some questions - In 10 years you will be ?"
+# 	answer1 = gets.chomp
 
-	puts "In 20 years you will be ?"
-	answer2 = gets.chomp
+# 	puts "In 20 years you will be ?"
+# 	answer2 = gets.chomp
 
-	puts "In 30 years you will be ?"
-	answer2 = gets.chomp
+# 	puts "In 30 years you will be ?"
+# 	answer2 = gets.chomp
 
-	puts "In 40 years you will be ?"
-	answer2 = gets.chomp
+# 	puts "In 40 years you will be ?"
+# 	answer2 = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "In 10 years you will be:"
+puts age + 10
+
+puts "In 20 years you will be:"
+puts age + 20
+
+puts "In 30 years you will be:"
+puts age + 30
+
+puts "In 40 years you will be:"
+puts age + 40	
