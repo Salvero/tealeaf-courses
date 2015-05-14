af5d4ae83ca015d23ae8970ed902625e0cf0dc29
Salman Akram <thunder9ruby@gmail.com>
2015-05-13 23:59:55 -0400

    add variables

---
+++ 2_Variables/age.rb
@@ -0,0 +1,14 @@
+puts "Hi, how old are you?"
+answer = gets.chomp
+
+	puts "great.  I have some questions - In 10 years you will be ?"
+	answer1 = gets.chomp
+
+	puts "In 20 years you will be ?"
+	answer2 = gets.chomp
+
+	puts "In 30 years you will be ?"
+	answer2 = gets.chomp
+
+	puts "In 40 years you will be ?"
+	answer2 = gets.chomp
---
+++ 2_Variables/name.rb
@@ -0,0 +1,9 @@
+puts "Your name is ......?"
+name = gets.chomp
+puts "Okay #{name.capitalize}"
+
+puts ""
+
+puts "so what is your planning to do in this summer ?!"
+message = gets.chomp
+puts "Hmmm really #{message.upcase}....that's great idea, we can do that! :)"
