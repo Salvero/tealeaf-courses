59cff4943368e7273221d62dfd3ee0fd00f22d85
Salman Akram <thunder9ruby@gmail.com>
2015-05-13 23:24:16 -0400

    completed exercises 3, 4, 5, 6

---
+++ 1_Basics/exercise_3.rb
@@ -0,0 +1,11 @@
+movies = { spiderman: 2003, 
+					 wild_card: 2015, 
+					 transformers: 2008, 
+					 mad_city: 1975, 
+					 notebook: 2002 }
+
+puts movies[:spiderman]
+puts movies[:wild_card]
+puts movies[:transformers]
+puts movies[:mad_city]
+puts movies[:notebook]
---
+++ 1_Basics/exercise_4.rb
@@ -0,0 +1,8 @@
+movies_titles = [1975, 2004, 2013, 2001, 1981, 2018, 1935, 1844]
+
+puts movies_titles[0]
+puts movies_titles[1]
+puts movies_titles[2]
+puts movies_titles[3]
+puts movies_titles[4]
+puts movies_titles[5]
